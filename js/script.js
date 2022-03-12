a27a45a4b9e94dada4aa14f489418347f756cfbc
Wanjiru393 <carolciru393@gmail.com>
2022-03-12 12:08:38 +0300

    Addition of click function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 
+$(document).ready(function(){
+  $(".road").hide();
+$(".croseover").hover(function(){
+    $(this).find(".road").toggle(400);
+});
+
+});
+$(".click1").click(function(){
+    $(".hide1").slideToggle(1000);
+    $(".show1").slideToggle(1200);
+  });
+
+  $(".click2").click(function(){
+    $(".hide2").slideToggle(1000);
+    $(".show2").slideToggle(1200);
+  });
+
+  $(".click3").click(function(){
+    $(".hide3").slideToggle(1000);
+    $(".show3").slideToggle(1200);
+  });
