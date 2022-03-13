0354f3d3081172c8672360af11977411cf6a7362
Wanjiru393 <carolciru393@gmail.com>
2022-03-13 18:02:08 +0300

    Addition of data collection and pop-up message function

--- js/script.js
+++ js/script.js
@@ -20,3 +20,15 @@ $(".click1").click(function(){
     $(".hide3").slideToggle(1000);
     $(".show3").slideToggle(1200);
   });
+$(document).ready(function(){
+    $('#submission').submit(function(event){
+        var display1 = $('input:name').val();
+        var display2 = $('input#email').val();
+        if(display1 && display2 != ''){
+            alert ('Thank you ' + display1 + 'for your subscription');
+        }else {
+            alert ('Invalid input');
+        }
+        event.preventDefault();
+    })
+});
